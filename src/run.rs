c5b164280bc4a846a2df5305b157198375b67a99
icit <38162697+ic-it@users.noreply.github.com>
2023-04-03 16:53:29 +0200

    RamCliStdin, RamCliStdout

--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,4 @@
+use std::io::{Write};
 use std::io::BufRead;
 use std::path::PathBuf;
 use std::io::BufReader;
@@ -8,12 +9,40 @@ use colored::Colorize;
 
 use crate::display_error::display_runtime_error;
 
-
 pub enum RunStatusCodes {
     Success,
     Error,
 }
 
+struct RamCliStdin;
+struct RamCliStdout;
+
+impl std::io::Read for RamCliStdin {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        print!("{}", ">>> ".cyan().bold());
+        stdout().flush().unwrap();
+        stdin().read(buf)
+    }
+}
+
+impl std::io::Write for RamCliStdout {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        print!("{}", "<<< ".cyan().bold());
+        stdout().write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        stdout().flush()
+    }
+}
+
+fn ramcli_stdin() -> RamCliStdin {
+    RamCliStdin
+}
+
+fn ramcli_stdout() -> RamCliStdout {
+    RamCliStdout
+}
 
 pub fn run (
     file: PathBuf,
@@ -24,14 +53,14 @@ pub fn run (
         let input_file = std::fs::File::open(input).expect("Unable to open input file");
         Box::new(BufReader::new(input_file))
     } else {
-        Box::new(BufReader::new(stdin()))
+        Box::new(BufReader::new(ramcli_stdin()))
     };
 
     let output_buf: Box<dyn std::io::Write> = if let Some(output) = output {
         let output_file = std::fs::File::create(output).expect("Unable to open output file");
         Box::new(BufWriter::new(output_file))
     } else {
-        Box::new(BufWriter::new(stdout()))
+        Box::new(BufWriter::new(ramcli_stdout()))
     };
 
     let source = std::fs::read_to_string(file).expect("Unable to read file");
