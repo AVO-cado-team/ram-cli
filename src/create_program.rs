21eaea44138de671f0046e55b8062a994d0cba53
icit <38162697+ic-it@users.noreply.github.com>
2023-04-04 15:39:56 +0200

    Beautiful code

--- src/create_program.rs
+++ src/create_program.rs
@@ -3,9 +3,8 @@ use std::path::PathBuf;
 use ramemu::parser;
 use ramemu::stmt::Stmt;
 use ramemu::program::Program;
-use ramemu::errors::ParseError;
 
-use crate::errors::RamCliError;
+use crate::errors::{RamCliError};
 use crate::display_error::display_parsing_error;
 
 pub fn create_program(
@@ -13,33 +12,27 @@ pub fn create_program(
     file_path: PathBuf,
 ) -> Result<Program, RamCliError> {
     let stmts_result: Vec<_> = parser::parse(source).collect();
-    let errors: Vec<ParseError> = stmts_result
+    let errors: Vec<_> = stmts_result
         .iter()
-        .filter_map(|stmt|
-            if let Err(e) = stmt {
-                display_parsing_error(source, e).ok()?;
-                Some(e.clone())
-            } else {
-                None
-            }
-        )
+        .filter_map(|stmt| stmt.as_ref().err().cloned())
         .collect();
-
-    if !errors.is_empty() {
-        return Err(RamCliError::Parse(
-            format!(
-                "Found {} {} in file: {}",
-                errors.len().to_string().bright_yellow().bold(),
-                if errors.len() == 1 { "error" } else { "errors" },
-                file_path
-                    .to_str()
-                    .expect("This should never happen")
-                    .bright_yellow()
-                    .bold()
-            )
-        ))
+    errors.iter().try_for_each(|error| display_parsing_error(source, error))?;
+    
+    if errors.is_empty() {
+        let stmts: Vec<Stmt> = stmts_result.into_iter().filter_map(|stmt| stmt.ok()).collect();
+        return Ok(Program::from(stmts));
     }
 
-    let stmts: Vec<Stmt> = stmts_result.into_iter().filter_map(|stmt| stmt.ok()).collect();
-    Ok(Program::from(stmts))
+    Err(RamCliError::Parse(
+        format!(
+            "Found {} {} in file: {}",
+            errors.len().to_string().bright_yellow().bold(),
+            if errors.len() == 1 { "error" } else { "errors" },
+            file_path
+                .to_str()
+                .expect("This should never happen")
+                .bright_yellow()
+                .bold()
+        )
+    ))
 }
