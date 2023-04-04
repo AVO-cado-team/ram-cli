b0a9e6be01d95d292c65f8f6cba8dbca24bc8d9f
icit <38162697+ic-it@users.noreply.github.com>
2023-04-04 15:19:52 +0200

    Fix obviously but ugly code

--- src/create_program.rs
+++ src/create_program.rs
@@ -1,45 +1,45 @@
-use ramemu::parser;
-use ramemu::program::Program;
-use std::path::PathBuf;
 use colored::Colorize;
+use std::path::PathBuf;
+use ramemu::parser;
+use ramemu::stmt::Stmt;
+use ramemu::program::Program;
+use ramemu::errors::ParseError;
 
 use crate::errors::RamCliError;
 use crate::display_error::display_parsing_error;
-
 
 pub fn create_program(
     source: &str,
     file_path: PathBuf,
 ) -> Result<Program, RamCliError> {
-    let program: Vec<_> = parser::parse(source).collect();
-
-    let errors = program
+    let stmts_result: Vec<_> = parser::parse(source).collect();
+    let errors: Vec<ParseError> = stmts_result
         .iter()
-        .filter_map(|stmt| stmt.as_ref().err().map(|e| display_parsing_error(source, e)))
-        .count();
-    
-    if errors == 0 {
-        return Ok(
-            Program::from(
-                program
-                    .into_iter()
-                    .map(|stmt| stmt.expect("Should be no errors (checked above)"))
-                    .collect()
-            )
+        .filter_map(|stmt|
+            if let Err(e) = stmt {
+                display_parsing_error(source, e).ok()?;
+                Some(e.clone())
+            } else {
+                None
+            }
         )
-    }
-    
-    Err(RamCliError::Parse(
-        format!(
-            "Found {} {} in file: {}",
-            errors.to_string().bright_yellow().bold(),
-            if errors == 1 { "error" } else { "errors" },
-            file_path
-                .to_str()
-                .expect("That shouldn't happen. Please report this bug.")
-                .bright_yellow()
-                .bold()
+        .collect();
 
-        )
-    ))
+    if !errors.is_empty() {
+        return Err(RamCliError::Parse(
+            format!(
+                "Found {} {} in file: {}",
+                errors.len().to_string().bright_yellow().bold(),
+                if errors.len() == 1 { "error" } else { "errors" },
+                file_path
+                    .to_str()
+                    .expect("This should never happen")
+                    .bright_yellow()
+                    .bold()
+            )
+        ))
+    }
+
+    let stmts: Vec<Stmt> = stmts_result.into_iter().filter_map(|stmt| stmt.ok()).collect();
+    Ok(Program::from(stmts))
 }
